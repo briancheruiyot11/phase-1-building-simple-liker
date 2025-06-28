98ccd145c6da03eb30118604ecfbb43bdb79e382
Brian Cheruiyot <briancheru11@gmail.com>
2025-06-28 23:25:08 +0300

    complete lab

--- main.js
+++ main.js
@@ -5,6 +5,42 @@ const FULL_HEART = '♥'
 // Your JavaScript code goes here!
 
 
+document.addEventListener('DOMContentLoaded', () => {
+  const hearts = document.querySelectorAll('.like-glyph');
+  const modal = document.getElementById('modal');
+  const modalMessage = document.getElementById('modal-message');
+
+  // hidden class to modal 
+  if (modal) {
+    modal.classList.add('hidden');
+  }
+
+  // click listener to each heart
+  hearts.forEach(heart => {
+    heart.addEventListener('click', () => {
+      mimicServerCall()
+        .then(() => {
+          if (heart.textContent === EMPTY_HEART) {
+            heart.textContent = FULL_HEART;
+            heart.classList.add('activated-heart');
+          } else {
+            heart.textContent = EMPTY_HEART;
+            heart.classList.remove('activated-heart');
+          }
+        })
+        .catch(error => {
+          if (modal) {
+            modal.classList.remove('hidden');
+            modalMessage.textContent = error;
+
+            setTimeout(() => {
+              modal.classList.add('hidden');
+            }, 3000);
+          }
+        });
+    });
+  });
+});
 
 
 //------------------------------------------------------------------------------
